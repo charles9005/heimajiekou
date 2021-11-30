42bb29f3bfb468d2eac02f3a407d0036a98cc45f
Charles.Lin <626853099@qq.com>
2021-11-30 14:57:23 +0800

    Initied commit

--- run_suite.py
+++ run_suite.py
@@ -9,4 +9,5 @@ suit.addTest(unittest.makeSuite(TestLogin))
 report_file ="./report/report{}.html".format(time.strftime("%Y%m%d_%H%M%S"))
 with open(report_file,"wb") as f:
     runner =HTMLTestRunner(f,title="charles接口自动化测试报告",description="V0.1")
+    #testsuite
     runner.run(suit)
